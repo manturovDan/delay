65390369fb007a09587979286075599ff03c71f3
Danila.Manturov <danila.manturov@jetbrains.com>
2021-11-15 18:34:12 +0300

    create long build

---
+++ LongBuild/src/LongBuild.java
@@ -0,0 +1,19 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class LongBuild {
+    public static void main(String[] args) throws InterruptedException {
+        int delayMinutes = Integer.parseInt(args[0]);
+        System.out.println("Delay for: " + delayMinutes + " m");
+        System.out.println(new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
+
+        int tenSecsIntervals = delayMinutes * 60 / 10;
+        for (int i = 0; i < tenSecsIntervals; ++i) {
+            System.out.println((i * 10) + "s/" + delayMinutes + "m");
+            TimeUnit.SECONDS.sleep(10);
+        }
+        System.out.println("The end!");
+        System.out.println(new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()));
+    }
+}
