524d28fa2e339d13fc6c7ce98243001203e5f59f
Danila.Manturov <danila.manturov@jetbrains.com>
2022-04-01 00:37:59 +0300

    JC PX-1 21

--- LongBuild/src/LongBuild.java
+++ LongBuild/src/LongBuild.java
@@ -18,4 +18,4 @@ public class LongBuild {
     }
 }
 
-//comment for commit 1234 px1 123 5 6 789898 123 fsdfsdf dsffasdfadssdfsfddsasfsdffsdf fsd das fsdfsd dasfddassf fsfsdf dsada dadasd gdfg dsadasd dasdasd
+//comment for commit 1234 px1 123 5 6 789898 123 fsdsaddfsdf dsffasdfadssdfsfddsasfsdffsdf fsd das fsdfsd dasfddassf fsfsdf dsada dadasd gdfg dsadasd dasdasd
