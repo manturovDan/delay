2e798eb26efd03deee39fe0481067251eff52141
Danila.Manturov <danila.manturov@jetbrains.com>
2022-04-13 01:51:14 +0300

    issue #1 and #2

--- LongBuild/src/LongBuild.java
+++ LongBuild/src/LongBuild.java
@@ -18,4 +18,4 @@ public class LongBuild {
     }
 }
 
-//comment for commit 1234 px1 123 5 6 789выфвы898 123 fsmjkljdsaddfsdf dsfdasdasdasfasdfadssdfsfddsasfsdffsdf fsd das fsdfsd dasfddassf fsfsdf dsada dadasd gdfg dsadasd dasdasd
+//comment for commit 1234 px1 123 5 6 789выфвы898 dasd 123 fsmjkljdsaddfsdf dsfdasdasdasfasdfadssdfsfddsasfsdffsdf fsd das fsdfsd dasfddassf fsfsdf dsada dadasd gdfg dsadasd dasdasd
